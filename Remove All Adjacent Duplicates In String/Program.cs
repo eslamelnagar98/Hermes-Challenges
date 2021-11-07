48abfeb30babfae43b727b4f0c831ceef406160d
Islam <eslamelnagar44@gmail.com>
2021-11-08 00:47:12 +0200

    Add project files.

---
+++ Remove All Adjacent Duplicates In String/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_All_Adjacent_Duplicates_In_String
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Word You Need To Check ");
+            var inputWordFromUser = Console.ReadLine();
+            var removeDuplicate = await RemoveDuplicates($@"The New Output From You Word Without Any Duplication Is :: {inputWordFromUser}");
+            Console.WriteLine(removeDuplicate);
+        }
+
+        public async static Task<string> RemoveDuplicates(string sentense)
+        {
+            if (sentense is null) return null;
+            var manyWordsInput = sentense.Split(' ');
+            //mutable string (does not create new object every time we add item to it )
+            var newWordWithoutDuplicate = new StringBuilder();
+            var newSentenseToReturn = new StringBuilder();
+            var wordIsAlreadHasNoDuplication = false;
+            bool isLenghtOfNewStringToReturnEqualZero = false;
+            foreach (var word in manyWordsInput)
+            {
+                wordIsAlreadHasNoDuplication = false;
+                newWordWithoutDuplicate.Remove(0, newWordWithoutDuplicate.Length);
+                if (word.Length < 2)
+                {
+                    wordIsAlreadHasNoDuplication = true;
+                    newWordWithoutDuplicate.Append(word);
+                }
+
+                for (int i = 0; i < word?.Length && !wordIsAlreadHasNoDuplication; i++)
+                {
+                    isLenghtOfNewStringToReturnEqualZero = newWordWithoutDuplicate.Length != 0;
+                    if (isLenghtOfNewStringToReturnEqualZero && (newWordWithoutDuplicate[newWordWithoutDuplicate.Length - 1] == word[i]))
+                        newWordWithoutDuplicate.Remove(newWordWithoutDuplicate.Length - 1, 1);
+                    else
+                        newWordWithoutDuplicate?.Append(word[i]);
+                }
+                if (newSentenseToReturn.Length != 0)
+                {
+                    newSentenseToReturn.Append(" ");
+                    newSentenseToReturn.Append(newWordWithoutDuplicate);
+                }
+                else
+                {
+                    newSentenseToReturn.Append(newWordWithoutDuplicate);
+                }
+
+            }
+
+
+            return newSentenseToReturn.ToString();
+        }
+    }
+}
---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanToInteger
+{
+    class Program
+    {
+        public static Dictionary<char, int> romanNumberDictionary = new Dictionary<char, int>
+        {
+            {'I',1 },
+            {'V',5 },
+            {'X',10 },
+            {'L',50 },
+            {'C',100 },
+            {'D',500 },
+            {'M',1000 }
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("IV"));
+            Console.WriteLine(RomanToInt("III"));
+            Console.WriteLine(RomanToInt("XLIX"));
+            Console.WriteLine(RomanToInt("LVIII"));
+            Console.WriteLine(RomanToInt("MCMXCIV"));
+        }
+
+        static int RomanToInt(string romanNumber)
+        {
+
+            var decimalNumbers = 0;
+            var lastElementAppear = 0;
+            foreach (var item in romanNumber)
+            {
+                romanNumberDictionary.TryGetValue(item, out int number);
+                if ((lastElementAppear != 0) && (number > lastElementAppear))
+                {
+                    decimalNumbers -= lastElementAppear;
+                    number -= lastElementAppear;
+                    decimalNumbers += number;
+                }
+                else
+                {
+                    decimalNumbers += number;
+                    lastElementAppear = number;
+                }
+            }
+
+            return decimalNumbers;
+        }
+    }
+}
